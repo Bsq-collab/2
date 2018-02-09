282f71a79d653199a12dadeba0847a83b1525c2c
Bayan Berri <bberri@stuy.edu>
2018-02-09 00:15:10 -0500

    done

--- animate.js
+++ animate.js
@@ -9,13 +9,16 @@ var ctx=c.getContext("2d");//obj info
 //Getting the button values
 var stop=document.getElementById("stop");
 var animate=document.getElementById("animate");
-
+var dvd_anim=document.getElementById("dvd");
 
 var radius=1;//startsat one and increments
 
 var reqID;//request ID useful for stopping
 var growth=true;//state variable
 var color=0//C0l0r3$
+
+//for the dvd part:
+var dvd=new Image();
 
 
 var changeState=function(radius){
@@ -52,9 +55,40 @@ var circle= function(){
   console.log("reqID==: "+ reqID);
 }
 
+var animateDVD = function() {
+  //declares the variables
+  var radius = 40;
+  var x = 300;
+  var y = 300
+  var dx = Math.floor(Math.random() * 3) + 1;
+  var dy = Math.floor(Math.random() * 3) + 1;
+  //uses the variables declared above to draw
+  var drawDVD = function() {
+    ctx.fillStyle="hsl(" + color++ + ",100%,50%)";
+    ctx.beginPath();
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    reqID = window.requestAnimationFrame(drawDVD);
+
+    x += dx;
+    y += dy;
+    console.log("x,y: " +x+","+y);
+    if (x <= radius || x >= 600 - radius) {
+      dx = (Math.random() - 0.3)-dx;
+    }
+    if (y <= radius || y >= 600 - radius) {
+      dy = (Math.random() - 0.3)-dy;
+    }
+  };
+
+  drawDVD();
+};
 
 var stopAnimation=function(){
   window.cancelAnimationFrame(reqID);//stops it
 }
 animate.addEventListener("click",circle);
+dvd_anim.addEventListener("click",animateDVD);
 stop.addEventListener("click",stopAnimation);
